4de62887ff521acaa786ef2f418d389722c80725
Chiranjeevi Dasatti <chiranjeevi.dasatti@planonsoftware.com>
2019-08-23 16:22:05 +0530

    added protractor confgiuration and simple test

---
+++ tests/app.e2e-spec.ts
@@ -0,0 +1,17 @@
+import { browser } from 'protractor'
+
+describe('core App', () => {
+
+    /// Create a driver for device
+    beforeAll(() => {
+        // Configuration for Allure Reports
+    });
+
+    beforeEach(() => {
+    });
+
+    it('Sample Test Case', () => {
+        browser.waitForAngularEnabled(true);
+    });
+
+});
